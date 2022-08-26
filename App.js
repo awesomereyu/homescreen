ae935700a180a90e5e407c3bd8b5091f2b63aade
awesomereyu <66564889+awesomereyu@users.noreply.github.com>
2022-08-26 14:30:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Homepages!</Text>
+      <Text>Homepage!</Text>
       <StatusBar style="auto" />
     </View>
   );
